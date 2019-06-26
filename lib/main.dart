68b9b38003226b97ac3866f7e33816ddfb13c9cd
Sanga Yoganathan <sanga.yoganathan@wishabi.com>
2019-06-26 01:12:40 -0400

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sri Vidya Temple App',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'Sri Vidya Temple'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+    Container templeInfo = Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        "Information about the temple",
+        textAlign: TextAlign.center,
+        style: Theme.of(context).textTheme.display1,
+      )
+    );
+
+    DefaultTabController controller = DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.calendar_today)),
+              Tab(icon: Icon(Icons.info)),
+              Tab(icon: Icon(Icons.mail)),
+              Tab(icon: Icon(Icons.photo)),
+              Tab(icon: Icon(Icons.folder_open)),
+            ],
+          ),
+          title: Text('Sri Vidya Temple'),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.calendar_today),
+            templeInfo,
+            Icon(Icons.mail),
+            Icon(Icons.photo),
+            Icon(Icons.folder),
+          ],
+        ),
+      ),
+    );
+
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return controller;
+  }
+}
