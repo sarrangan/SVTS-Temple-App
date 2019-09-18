57d358fe8b32411d6411babdfec6cee3018af472
Sanga Yoganathan <sanga.yoganathan@wishabi.com>
2019-09-17 20:42:22 -0400

    Some firebase stuff to store passwords

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/sangayoganathan/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/sangayoganathan/svts-temple-app"
+export "FLUTTER_TARGET=/Users/sangayoganathan/svts-temple-app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/sangayoganathan/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
